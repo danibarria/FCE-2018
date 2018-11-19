953e4d03e51d9920380cad27b78d4a5f15bb4c25
Andres Santillan <afs9719@gmail.com>
2018-11-19 20:36:20 -0300

    agregado de insert

---
+++ SQL/insert.sql
@@ -0,0 +1,46 @@
+insert into Localidad(nombre) values 
+	("Trelew");
+
+insert into Localidad(nombre) values 
+	("Rawson");
+
+insert into Localidad(nombre) values 
+	("Puerto Madryn");
+
+insert into Localidad(nombre) values 
+	("Tecka");
+
+insert into Aspirante(nombre, apellido, direccion) values
+	("Carlos", "Irigoyen", "Pietroveli 173");
+
+insert into Aspirante(nombre, apellido, direccion) values
+	("Pewen", "Sugar", "Cuba 908");
+
+insert into Aspirante(nombre, apellido, direccion) values
+	("Jose", "Dama", "Calle Falsa 123");
+
+insert into Aspirante(nombre, apellido, direccion) values
+	("Elsa", "Biondo", "Mitre 124");
+
+insert into Aspirante(nombre, apellido, direccion) values
+	("Merli", "Bergeron", "Marconi 200");
+
+insert into Docente(fecha_nacimiento, nombre, apellido, direccion, id_aspirante, id_localidad) values
+	("Juan", "Minujin", "Winter 33", 1, 1);
+
+insert into Docente(fecha_nacimiento, nombre, apellido, direccion, id_aspirante, id_localidad) values
+	("Pablo", "Neruda", "Pecoraro 1432", 2, 1);
+
+insert into Docente(fecha_nacimiento, nombre, apellido, direccion, id_aspirante, id_localidad) values
+	("Andres", "Ovich, "San Martin 285", 3, 2);
+
+
+
+
+
+
+
+
+
+
+
