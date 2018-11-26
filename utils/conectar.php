0f090a1bb117a09b40abee880e7a3ffb7d0813e2
danibarria <barriareyesjosedaniel@gmail.com>
2018-11-26 17:59:25 -0300

    insecure change

--- utils/conectar.php
+++ utils/conectar.php
@@ -1,9 +1,9 @@
 <?php
 
 function conectar(){
-    define("DB_HOST",$_ENV['DB_CLEARDB_DATABASE_URL'] );  
-    define("DB_USER", $_ENV['DB_USER']);  
-    define("DB_PASS", $_ENV['DB_PASSWORD']);  
+    define("DB_HOST","mysql://b38793a56ebb32:c7661958@us-cdbr-iron-east-01.cleardb.net/heroku_cfa203353792999?reconnect=true" );  
+    define("DB_USER", "b38793a56ebb32");  
+    define("DB_PASS", "c7661958");  
     define("DB_DATABASE", "heroku_cfa203353792999" );  
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);   
     if($mysqli->connect_errno > 0){   
