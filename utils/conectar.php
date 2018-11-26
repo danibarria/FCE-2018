91c807773646a445bfdeab258f49cda07767ce58
danibarria <barriareyesjosedaniel@gmail.com>
2018-11-26 18:00:45 -0300

    insecure change

--- utils/conectar.php
+++ utils/conectar.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectar(){
-    define("DB_HOST","mysql://b38793a56ebb32:c7661958@us-cdbr-iron-east-01.cleardb.net/heroku_cfa203353792999?reconnect=true" );  
+    define("DB_HOST","us-cdbr-iron-east-01.cleardb.net" );  
     define("DB_USER", "b38793a56ebb32");  
     define("DB_PASS", "c7661958");  
     define("DB_DATABASE", "heroku_cfa203353792999" );  
