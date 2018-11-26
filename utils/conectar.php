314e137d482d1bf06cc7154bfef74d22c33503b3
danibarria <barriareyesjosedaniel@gmail.com>
2018-11-26 18:51:44 -0300

    eliminated public key

--- utils/conectar.php
+++ utils/conectar.php
@@ -1,9 +1,9 @@
 <?php
 
 function conectar(){
-    define("DB_HOST","us-cdbr-iron-east-01.cleardb.net" );  
-    define("DB_USER", "b38793a56ebb32");  
-    define("DB_PASS", "c7661958");  
+    define("DB_HOST","" );  
+    define("DB_USER", "");  
+    define("DB_PASS", "");  
     define("DB_DATABASE", "heroku_cfa203353792999" );  
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);   
     if($mysqli->connect_errno > 0){   
