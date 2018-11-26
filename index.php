bda3805790f638964756613d17bbecdeae13e6e9
danibarria <barriareyesjosedaniel@gmail.com>
2018-11-26 16:07:52 -0300

    index creado

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<div class="container">
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>FCE - 2018</title>
+  </head>
+
+
+  <div class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
   <a class="navbar-brand" href="#">FCE-NO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,7 +22,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
 
       <!-- <li class="nav-item dropdown">
@@ -27,10 +41,10 @@
           Designacion
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="../Designacion/alta-Designacion.php">Alta</a>
+          <a class="dropdown-item" href="Designacion/alta-Designacion.php">Alta</a>
           <a class="dropdown-item" href="#">Baja</a>
           <!-- <div class="dropdown-divider"></div> -->
-          <a class="dropdown-item" href="../Designacion/consulta-Designacion.php">Consulta</a>
+          <a class="dropdown-item" href="Designacion/consulta-Designacion.php">Consulta</a>
         </div>
       </li>
       <li class="nav-item dropdown">
@@ -38,10 +52,10 @@
           Concurso
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="../concurso/alta-concurso.php">Alta</a>
+          <a class="dropdown-item" href="concurso/alta-concurso.php">Alta</a>
           <a class="dropdown-item" href="#">Baja</a>
           <!-- <div class="dropdown-divider"></div> -->
-          <a class="dropdown-item" href="../concurso/consulta-concurso.php">Consulta</a>
+          <a class="dropdown-item" href="concurso/consulta-concurso.php">Consulta</a>
         </div>
       </li>
       <li class="nav-item dropdown">
@@ -49,10 +63,10 @@
           Aspirante
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="../aspirante/alta-aspirante.php">Alta</a>
+          <a class="dropdown-item" href="aspirante/alta-aspirante.php">Alta</a>
           <a class="dropdown-item" href="#">Baja</a>
           <!-- <div class="dropdown-divider"></div> -->
-          <a class="dropdown-item" href="../aspirante/consulta-aspirante.php">Consulta</a>
+          <a class="dropdown-item" href="aspirante/consulta-aspirante.php">Consulta</a>
         </div>
       </li>
       <li class="nav-item dropdown">
@@ -60,10 +74,10 @@
           Docente
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="../docente/alta-docente.php">Alta</a>
+          <a class="dropdown-item" href="docente/alta-docente.php">Alta</a>
           <a class="dropdown-item" href="#">Baja</a>
           <!-- <div class="dropdown-divider"></div> -->
-          <a class="dropdown-item" href="../docente/consulta-docente.php">Consulta</a>
+          <a class="dropdown-item" href="docente/consulta-docente.php">Consulta</a>
         </div>
       </li>
 
@@ -72,10 +86,10 @@
           Localidad
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="../localidad/alta-localidad.php">Alta</a>
+          <a class="dropdown-item" href="localidad/alta-localidad.php">Alta</a>
           <a class="dropdown-item" href="#">Baja</a>
           <!-- <div class="dropdown-divider"></div> -->
-          <a class="dropdown-item" href="../localidad/consulta-localidad.php">Consulta</a>
+          <a class="dropdown-item" href="localidad/consulta-localidad.php">Consulta</a>
         </div>
       </li>
     </ul>
@@ -85,3 +99,13 @@
     </form>
   </div>
 </nav>
+<section>
+<img style="width:100%;" src="assets/landing-image.jpg"/>
+</section>
+</div>
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
