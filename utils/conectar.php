5574adbbd3147265e175335386f3eb239b6e1c7e
danibarria <barriareyesjosedaniel@gmail.com>
2018-11-26 17:21:29 -0300

    conectar modificado

--- utils/conectar.php
+++ utils/conectar.php
@@ -1,10 +1,10 @@
 <?php
 
 function conectar(){
-    define("DB_HOST","localhost" );  
-    define("DB_USER", "root");  
-    define("DB_PASS", "");  
-    define("DB_DATABASE", "fce" );  
+    define("DB_HOST",$_ENV['DB_CLEARDB_DATABASE_URL'] );  
+    define("DB_USER", $_ENV['DB_USER']);  
+    define("DB_PASS", $_ENV['DB_PASSWORD']);  
+    define("DB_DATABASE", "heroku_cfa203353792999" );  
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);   
     if($mysqli->connect_errno > 0){   
         die("Imposible conectarse con la base de datos [" . $mysqli->connect_error . "]");   
