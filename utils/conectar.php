b9ac611e312b0323ffbf988942bc6f44fa9c63aa
danibarria <barriareyesjosedaniel@gmail.com>
2018-11-26 17:36:32 -0300

    debug

--- utils/conectar.php
+++ utils/conectar.php
@@ -7,7 +7,7 @@ function conectar(){
     define("DB_DATABASE", "heroku_cfa203353792999" );  
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);   
     if($mysqli->connect_errno > 0){   
-        die("Imposible conectarse con la base de datos [" . $mysqli->connect_error . "]");   
+        die("dani: " . DB_HOST."Imposible conectarse con la base de datos [" . $mysqli->connect_error . "]");   
     }  
     return $mysqli;
 }
